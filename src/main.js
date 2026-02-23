7cb0978a00c18915fa842eb9b5f2234c117d1e75
Lina-Chaparro <lpchaparroa@uniboyaca.edu.co>
2026-02-23 15:54:23 -0500

    lol

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 //Logica y conexion del api con fetch y ui
 //logica navegacion
+
+import {fetchMlp} from "./services/api.js";
+import {showMlp} from "./ui/ui.js";
+
+let current = 10;
+
+async function loadMlp(id){
+    const mlp = await fetchMlp(id);
+    showMlp(mlp);
+}
+
+loadMlp(current);
+
+document.querySelector("#next").addEventListener("click", () => {
+    current++;
+    loadMlp(mlp);
+});
+
+document.querySelector("#prev").addEventListener("click", () => {
+    if (current >1) current--;
+    loadMlp(current);
+});
--- src/services/api.js
+++ src/services/api.js
@@ -5,3 +5,25 @@
 import Mlp from "../models/Mlp.js";
 
 const API_URL = "http://ponyapi.net/v1/";
+export async function fetchMlp(id){
+    try{
+        const res =await fetch (API_URL + id);
+        if (!res.ok) throw new Error("No se encontró el pony");
+        const data= await res.json();
+
+        //Extraer kinds
+        const kinds = data.kinds.map(k => k.kinds.name);
+
+        //instance
+        return new Mlp(
+            data.id,
+            data.name,
+            kinds,
+            data.image.other["official-artwork"].front_default
+        );
+
+    }catch (error) {
+        console.error(error);
+        return null;
+    }
+}
