db91546377ecb416d4b1598a3744c4a63add9f74
Lina-Chaparro <lpchaparroa@uniboyaca.edu.co>
2026-02-23 15:53:58 -0500

    avance 2

--- src/models/Mlp.js
+++ src/models/Mlp.js
@@ -1,1 +1,11 @@
 //Exportar clase base: información de cada mlp//
+
+//No es necesario que este en orden como el resource
+export default class Mlp {
+    constructor(id,name,kind,image){
+        this.id = id;
+        this.name = name;
+        this.kind = kind;
+        this.image = image;
+    }
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,7 @@
-//Métodos
+//Metodos
+// # para id
+// . para clase
+
+import Mlp from "../models/Mlp.js";
+
+const API_URL = "http://ponyapi.net/v1/";
